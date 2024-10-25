1976f0cd03dd163cfab94b35f10b34f5620e1c89
Tengku Farisha <tengkufarisha10@gmail.com>
2024-10-25 16:04:51 +0800

    master setup

---
+++ resources/views/layouts/partials/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar fixed-top" style="background-color: aliceblue; min-height: 100px;">
+    <div class="container-fluid">
+        <div class="d-flex align-items-center">
+            <a class="navbar-brand ms-md-3" href="/home">
+                <img src="{{ asset('asset/photo/logoipg.png') }}" alt="ipg.jpeg" width="70" height="80">
+            </a>
+        </div>
+        <p class="h4 text-dark">IPGKI-edu Gather</p>
+        <div class="d-flex align-items-center">
+            <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/profile">Profile</a></li>
+                <li>
+                    
+                </li>
+            </ul>
+        </div>        
+    </div>
+</nav>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, unde! Dolorem, vitae sint excepturi in veritatis voluptate, odit qui rem facere, suscipit eaque quo quod maxime cumque voluptates ullam voluptatem.
+@endsection
